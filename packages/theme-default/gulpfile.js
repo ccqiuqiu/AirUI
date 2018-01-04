0fb0aff8094d3d7bf6211c078925a3a7c0865920
cc <cc--x@163.com>
2018-01-04 09:14:56 +0800

    修改主题使用less

--- packages/theme-default/gulpfile.js
+++ packages/theme-default/gulpfile.js
@@ -1,25 +1,26 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var cssmin = require('gulp-cssmin');
-var salad = require('postcss-salad')(require('./salad.config.json'));
+const gulp = require('gulp')
+const postcss = require('gulp-postcss')
+const cssmin = require('gulp-cssmin')
+const less = require('gulp-less')
 
 gulp.task('compile', function() {
-  return gulp.src('./src/*.css')
-    .pipe(postcss([salad]))
+  return gulp.src('./src/*.less')
+    .pipe(less())
+    .pipe(postcss())
     .pipe(cssmin())
-    .pipe(gulp.dest('../../lib/css'));
+    .pipe(gulp.dest('../../lib/css'))
 });
 
 gulp.task('copyfont', function() {
   return gulp.src('./src/fonts/**')
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib/fonts'));
+    .pipe(gulp.dest('./lib/fonts'))
 });
 
 
-gulp.task('build', ['compile', 'copyfont']);
+gulp.task('build', ['compile', 'copyfont'])
 gulp.task('watch', function () {
-  gulp.watch('./src/*.css', ['compile']);
-});
+  gulp.watch('./src/*.less', ['compile'])
+})
